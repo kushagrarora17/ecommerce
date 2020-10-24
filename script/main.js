2257fe7148824d8368e95627f3681a253830f1a8
Kushagr Arora <kushagr.arora@airtel.com>
2020-10-24 18:05:22 +0530

    add throttle to menu

--- script/main.js
+++ script/main.js
@@ -1,11 +1,52 @@
+// Utility Start
+
+function throttle(inputFn, delay) {
+  let lastTime = 0;
+  let timerRef = null;
+
+  return function (...args) {
+    let currentTime = new Date();
+    clearTimeout(timerRef);
+
+    if (currentTime.getTime() - lastTime > delay) {
+      inputFn.apply(null, args);
+      lastTime = currentTime.getTime();
+    } else {
+      timerRef = setTimeout(function () {
+        inputFn.apply(null, args);
+        lastTime = currentTime.getTime();
+      }, delay);
+    }
+  };
+}
+
+// Utility End
+
 let menuBar = document.getElementById("js-menu-bar");
+let menuOffset = menuBar.offsetTop;
+
+function setMenuBar() {
+  let currentScroll = document.documentElement.scrollTop;
+
+  if (currentScroll > menuOffset) {
+    if (!menuBar.classList.contains("fixed")) {
+      menuBar.classList.add("fixed");
+    }
+  } else {
+    if (menuBar.classList.contains("fixed")) {
+      menuBar.classList.remove("fixed");
+    }
+  }
+}
+
+const throttledSetMenuBar = throttle(setMenuBar, 500);
+
+window.addEventListener("scroll", throttledSetMenuBar);
 
 // let bannerContainer = document.getElementsByClassName("banner-container");
 // let sliderImg1 = document.getElementById("js-slider1");
 // let sliderImg2 = document.getElementById("js-slider2");
 // let sliderImg3 = document.getElementById("js-slider3");
-
-console.dir(menuBar);
 
 // window.addEventListener("scroll", function(ev) {
 //   let menuOffset = menuBar.offsetTop;
@@ -22,24 +63,11 @@ console.dir(menuBar);
 //   }
 // });
 
-setInterval( function () {
+// setInterval( function () {
 
-  let menuOffset = menuBar.offsetTop;
-  let currentScroll = document.documentElement.scrollTop;
-
-  if(currentScroll > menuOffset) {
-    if(!menuBar.classList.contains("fixed")) {
-      menuBar.classList.add("fixed");
-    }
-  } else {
-    if(menuBar.classList.contains("fixed")) {
-      menuBar.classList.remove("fixed");
-    }
-  }
-}, 500);
+//   , 500);
 
 // setTimeout(function(){
 //   var v = object.style.zIndex;
 
-  
 // },3000)
